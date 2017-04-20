623f915427d96ec8133766a285c6893dd19021ec
mikanette <lohmus.marika@gmail.com>
2017-04-20 15:45:01 -0400

    Overwrote Rohan Map R
    Added WINE!

--- Rohan_map.R
+++ Rohan_map.R
@@ -2,6 +2,13 @@ library('tidyverse')
 library('ggmap')
 
 manhattan_map <- get_map("Manhattan, NY", source="google", maptype="toner-lite", zoom=12, color="bw")
-liquor <- read.csv('Data/liquor_licenses/massaged_data.csv')
+liquor <- read.csv('Data/liquor_licenses/massaged_data.csv', header="TRUE")
 manhattan <- liquor %>% filter(city.x == 'Manhattan')
-ggmap(manhattan_map) + geom_point(data=manhattan, aes(x=Longitude, y=Latitude,color = nbh))
+
+liquor_class<-read.csv("Data/Liquor Licenses/liquor_classification.csv")
+colnames(liquor_class)<-c("License.Type.Name","Classification")
+liquor<-merge(liquor, liquor_class, by="License.Type.Name")
+
+manhattan <- liquor %>% filter(Classification == 'WINE')
+
+ggmap(manhattan_map) + geom_point(data=manhattan, aes(x=Longitude, y=Latitude, color = Classification))
